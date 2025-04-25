09dd36e90a5eecf69c4e11525d1eb4175ffdb60d
Michael Tuepker <93541888+Bobsled6@users.noreply.github.com>
2025-04-25 00:07:04 -0400

    added pseudologic for getting computers choice

---
+++ javascript.js
@@ -0,0 +1,12 @@
+/* 
+
+create function getComputerChoice
+generate random number between 0 and 1 (x)
+generate string variable y 
+set y equal to rock if x is less than one third
+set y equal to paper if x greater than or equal to one third but less than two thirds
+set y equal to scissors if x is greater than two thirds
+
+*/
+
+
